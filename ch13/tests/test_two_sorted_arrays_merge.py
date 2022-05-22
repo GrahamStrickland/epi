6c0b97d0805d30ec8c811948e78cd315d1842baa
Graham Strickland <grahamhstrickland@gmail.com>
2022-05-22 17:02:58 +0200

    Updated ch13 with two_sorted_arrays_merge.py and tests/test_two_sorted_arrays_merge.py; ran tests with nose

---
+++ ch13/tests/test_two_sorted_arrays_merge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from nose.tools import assert_equal
+from ..two_sorted_arrays_merge import merge_two_sorted_arrays
+
+
+def test_merge_two_sorted_arrays0() -> None:
+    A = [3,13,17,None,None,None,None]
+    B = [3,7,11,19]
+    merge_two_sorted_arrays(A, 3, B, 4)
+    exp = [3,3,7,11,13,17,19]
+    assert_equal(A, exp)
