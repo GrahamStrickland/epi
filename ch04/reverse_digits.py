7e3e0a75bfd40fc0138c1b29b8cb554e04654e33
Graham Strickland <grahamhstrickland@gmail.com>
2022-08-26 18:19:03 +0200

    Altered solution to reverse_digits; tests passed

--- ch04/reverse_digits.py
+++ ch04/reverse_digits.py
@@ -2,10 +2,8 @@
 
 
 def reverse_digits(x: int) -> int:
-    forward_string = str(x)
-    reverse_string = ""
-    sign = -1 if forward_string[0] == '-' else 1
-    for i in range(len(forward_string)-1, -1):
-        if forward_string[i].isalnum():
-            reverse_string += forward_string[i]
-    return int(reverse_string) * sign
+    result, x_remaining = 0, abs(x)
+    while x_remaining:
+        result = result * 10 + x_remaining % 10
+        x_remaining //= 10
+    return -result if x < 0 else result
