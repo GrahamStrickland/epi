e62c6888fbd5923b615791fa34df3c6a405ea7f4
Graham Strickland <grahamhstrickland@gmail.com>
2022-08-29 19:13:21 +0200

    Added int_as_array_increment.py; passed all tests

---
+++ ch05/int_as_array_increment.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+
+from typing import List
+
+
+def plus_one(A: List[int]) -> List[int]:
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    else:
+        if A[0] == 10:
+            # There is a carry-out, so we need one more digit to store the result.
+            # A slick way to do this is to append a 0 at the end of the array,
+            # and update the first entry to 1.
+            A[0] = 1
+            A.append(0)
+    return A
--- ch05/offline_sampling.py
+++ ch05/offline_sampling.py
@@ -4,9 +4,34 @@
 import random
 from typing import List
 
-
 def random_sampling(k: int, A: List[int]) -> None:
     for i in range(k):
         # Generate a random index in [i, len(A) - 1].
         r = random.randint(i, len(A) - 1)
         A[i], A[r] = A[r], A[i]
+
+
+def test_random_sampling0() -> List[int]:
+    k = 3
+    A = [3,7,5,11]
+    random_sampling(k, A)
+    return A
+
+
+def test_random_sampling1() -> List[int]:
+    k = 2
+    A = [2,4,6,8,10,12,14,16]
+    random_sampling(k, A)
+    return A
+
+
+def test_random_sampling2() -> List[int]:
+    k = 5
+    A = [1,2,3,5,7,11,13,17,19,23,29]
+    random_sampling(k, A)
+    return A
+
+
+print(test_random_sampling0())
+print(test_random_sampling1())
+print(test_random_sampling2())
