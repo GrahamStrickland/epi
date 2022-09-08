e700a86103eb9f1ee19ca35c4860f274c6efb42f
Graham Strickland <grahamhstrickland@gmail.com>
2022-09-08 18:13:16 +0200

    Updated dutch_flag_three_values; failed tests

--- ch05/dutch_national_flag_three_values.py
+++ ch05/dutch_national_flag_three_values.py
@@ -5,20 +5,13 @@ from typing import List
 
 
 def dutch_flag_three_values(A: List[int]) -> None:
-    val1, val2, val3 = A[0], float('inf'), float('inf')
-    i = j = 0
-    while A[i] == val1 and i < len(A):
-        if A[i] != val1 and A[i] != val2:
-            val2 = A[i]
-            j = i
-        else:
-            i += 1
-    while (A[i] == val1 or A[i] == val2) and i < len(A):
-        if A[i] == val1:
-            A[i], A[j] = A[j], A[i]
-            j += 1
-            i += 1
-        elif A[i] == val2:
-            i += 1
-        else:
-            val3 = A[i]
+    i = 1
+    first, last = 0, len(A) - 1
+    while i < len(A) - 1:
+        if A[i] == A[first]:
+            A[i], A[first + 1] = A[first + 1], A[i]
+            first += 1
+        elif A[i] == A[last]:
+            A[i], A[last - 1] = A[last - 1], A[i]
+            last -= 1
+        i += 1
