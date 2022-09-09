6a1b24784d14da15157aa0bc4d16bfe2309f75f0
Graham Strickland <grahamhstrickland@gmail.com>
2022-09-09 17:29:02 +0200

    Attempted debug; algorithm failed

--- ch05/dutch_national_flag_three_values.py
+++ ch05/dutch_national_flag_three_values.py
@@ -2,16 +2,32 @@
 
 
 from typing import List
+import pdb
 
 
 def dutch_flag_three_values(A: List[int]) -> None:
     i = 1
     first, last = 0, len(A) - 1
+    val1, val2, val3 = A[first], float('inf'), float('inf')
     while i < len(A) - 1:
-        if A[i] == A[first]:
+        if A[i] == val1:
             A[i], A[first + 1] = A[first + 1], A[i]
             first += 1
-        elif A[i] == A[last]:
-            A[i], A[last - 1] = A[last - 1], A[i]
-            last -= 1
+        else:
+            if val3 == float('inf'):
+                if val3 != A[last]:
+                    A[i], A[last] = A[last], A[i]
+                    i -= 1
+                val3 = A[i]
+            elif val2 == float('inf'):
+                val2 = A[i]
+            elif A[i] == val3:
+                A[i], A[last - 1] = A[last - 1], A[i]
+                last -= 1
         i += 1
+
+
+if __name__ == "__main__":
+    pdb.set_trace()
+    A = [1,0,2,1,0,2,1,0,2]
+    dutch_flag_three_values(A)
