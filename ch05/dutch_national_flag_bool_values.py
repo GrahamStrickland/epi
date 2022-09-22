b625fd211c6401ce58b0c3d203532146b4d59b79
Graham Strickland <grahamhstrickland@gmail.com>
2022-09-22 19:17:40 +0200

    Completed boolean flag partition; tests passed

--- ch05/dutch_national_flag_bool_values.py
+++ ch05/dutch_national_flag_bool_values.py
@@ -5,4 +5,11 @@ from typing import List
 
 
 def dutch_flag_bool(A: List[int]) -> None:
-    pass
+    first = 0
+    last = len(A) - 1
+    while first <= last:
+        if A[first]:
+            A[first], A[last] = A[last], A[first]
+            last -= 1
+        else:
+            first += 1
