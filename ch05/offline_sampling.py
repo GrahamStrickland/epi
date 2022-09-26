032593bf021ecba1b701e6f122de52e36e2d9f1f
Graham Strickland <grahamhstrickland@gmail.com>
2022-09-26 17:34:19 +0200

    Added extra test to offline_sampling.py

--- ch05/offline_sampling.py
+++ ch05/offline_sampling.py
@@ -11,27 +11,35 @@ def random_sampling(k: int, A: List[int]) -> None:
         A[i], A[r] = A[r], A[i]
 
 
-def test_random_sampling0() -> List[int]:
+def test_random_sampling() -> List[int]:
     k = 3
     A = [3,7,5,11]
     random_sampling(k, A)
     return A
 
 
-def test_random_sampling1() -> List[int]:
+def test_random_sampling2() -> List[int]:
     k = 2
     A = [2,4,6,8,10,12,14,16]
     random_sampling(k, A)
     return A
 
 
-def test_random_sampling2() -> List[int]:
+def test_random_sampling3() -> List[int]:
     k = 5
     A = [1,2,3,5,7,11,13,17,19,23,29]
     random_sampling(k, A)
     return A
 
 
-print(test_random_sampling0())
-print(test_random_sampling1())
+def test_random_sampling4() -> List[int]:
+    k = 5
+    A = list(range(100))
+    random_sampling(k, A)
+    return A
+
+
+print(test_random_sampling())
 print(test_random_sampling2())
+print(test_random_sampling3())
+print(test_random_sampling4())
